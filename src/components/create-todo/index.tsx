aa995f86b2f67ee3766567df345193ee933f0ee4
Anton Prokopenko <anton.9090@bk.ru>
2022-07-22 23:14:02 +0100

    test pre-commit hook

--- src/components/create-todo/index.tsx
+++ src/components/create-todo/index.tsx
@@ -10,9 +10,9 @@ function CreateTodo() {
 
     const createTodo = React.useCallback(
         (event: React.MouseEvent<HTMLButtonElement>) => {
-            event.preventDefault();
-            if (value) addTodo(value);
-            updateValue('');
+        event.preventDefault();
+        if (value) addTodo(value);
+        updateValue('');
         },
         [value, addTodo]
     );
@@ -27,7 +27,7 @@ function CreateTodo() {
     return (
         <form className="create-todo">
             <Input
-                placeholder="Write new task here!"
+                placeholder="Write new task here!!!"
                 value={value}
                 onChange={onChange}
                 id="type"
